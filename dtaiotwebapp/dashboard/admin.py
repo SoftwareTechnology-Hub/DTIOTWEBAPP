70f55362afad0a5e6f96840f171bf2ee788bb416
SoftwareTechnology-Hub <hii.intern.hii@gmail.com>
2026-02-11 17:16:54 +0530

    admin model add view all

--- dtaiotwebapp/dashboard/admin.py
+++ dtaiotwebapp/dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Custom_Dashboard, Custom_Feed, FeedData, DashboardWidget, WidgetData
+
+admin.site.register(Custom_Dashboard)
+admin.site.register(Custom_Feed)
+admin.site.register(FeedData)
+admin.site.register(DashboardWidget)
+admin.site.register(WidgetData)
