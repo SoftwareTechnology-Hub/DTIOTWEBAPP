6ed027a845306b13a5fd58c16639556aa9b14990
SoftwareTechnology-Hub <hii.intern.hii@gmail.com>
2026-02-12 15:19:38 +0530

    rate limit

--- dtaiotwebapp/dashboard/views.py
+++ dtaiotwebapp/dashboard/views.py
@@ -216,10 +216,7 @@ from django.http import JsonResponse
 import json
 from users.models import CustomUser
 from .models import Custom_Feed, FeedData
-
 @csrf_exempt
-@ratelimit(key='user_or_api_key', rate='10/10s', method='POST', block=True)
-
 def Feed_data(request):
     if request.method != 'POST':
         return JsonResponse({"error": "POST required"}, status=405)
@@ -345,7 +342,6 @@ def dashboard_data_json(request, slug):
 
 
 @csrf_exempt
-@ratelimit(key='user_or_api_key', rate='10/10s', method='POST', block=True)
 def dashboard_data(request):
     if request.method != "POST":
         return JsonResponse({"error": "POST only"}, status=405)
