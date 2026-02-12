01993bae6306ad1acf2b4a8a54c92d0996c9f03e
SoftwareTechnology-Hub <hii.intern.hii@gmail.com>
2026-02-12 11:08:26 +0530

    iisue slove

--- dtaiotwebapp/dtaiotwebapp/settings.py
+++ dtaiotwebapp/dtaiotwebapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-m)z&oc#+%%@)29&!_7l@qd@b7+(j&63l7(f^8z93j5rxe*+cg5"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
